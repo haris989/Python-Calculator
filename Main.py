c5503264e9ac933a99c9ec2b538aeef8b68d7c40
Anas Nadeem <anas5678go@gmail.com>
2020-10-03 13:48:10 +0530

    Improved the calculator by adding square and cube of number function and decrese the time to 3

--- Main.py
+++ Main.py
@@ -16,38 +16,66 @@ def multiply(num1, num2):
 def divide(num1, num2): 
 	return num1 / num2 
 
+# Square of a number
+def square_of_number(num1):
+	return num1 * num1
+
+# Cube of number
+def cube_of_number(num1):
+	return num1 * num1 * num1
+
 print("Please select operation -\n"
 		"1. Add\n"
 		"2. Subtract\n" 
 		"3. Multiply\n"
-		"4. Divide\n") 
+		"4. Divide\n" 
+		"5. Square of Number\n"
+		"6. Cube of Number\n") 
 
 
 # Taking input from the user 
-select = input("Select operations form 1, 2, 3, 4 :") 
+select = input("Select operations form 1, 2, 3, 4,5 :") 
 
-number_1 = int(input("Enter first number: ")) 
-number_2 = int(input("Enter second number: ")) 
-
-if select == '1': 
+if select == '1':	
+	number_1 = int(input("Enter first number: ")) 
+	number_2 = int(input("Enter second number: "))  
 	print(number_1, "+", number_2, "=", 
 					add(number_1, number_2))
-	time.sleep(5) #to show the results for 5 seconds before the program exits!
+	time.sleep(3) #to show the results for 3 seconds before the program exits!
 
 elif select == '2': 
+	number_1 = int(input("Enter first number: ")) 
+	number_2 = int(input("Enter second number: "))  
 	print(number_1, "-", number_2, "=", 
 					subtract(number_1, number_2)) 
-	time.sleep(5) #to show the results for 5 seconds before the program exits!
+	time.sleep(3) #to show the results for 3 seconds before the program exits!
 
 elif select == '3': 
+	number_1 = int(input("Enter first number: ")) 
+	number_2 = int(input("Enter second number: "))  
 	print(number_1, "*", number_2, "=", 
 					multiply(number_1, number_2)) 
-	time.sleep(5) #to show the results for 5 seconds before the program exits!
+	time.sleep(3) #to show the results for 3 seconds before the program exits!
 
 elif select == '4': 
+	number_1 = int(input("Enter first number: ")) 
+	number_2 = int(input("Enter second number: "))  
 	print(number_1, "/", number_2, "=", 
 					divide(number_1, number_2)) 
-	time.sleep(5) #to show the results for 5 seconds before the program exits!
+	time.sleep(3) #to show the results for 3 seconds before the program exits!
+
+elif select == '5': 
+	number_1 = int(input("Enter the number: ")) 
+	print(number_1, "*", number_1, "=", 
+					square_of_number(number_1)) 
+	time.sleep(3) #to show the results for 3 seconds before the program exits!
+
+elif select == '6': 
+	number_1 = int(input("Enter the number: ")) 
+	print(number_1, "*", number_1, "*", number_1, "=", 
+					cube_of_number(number_1)) 
+	time.sleep(3) #to show the results for 3 seconds before the program exits!
+
 else: 
 	print("Syntax Error!") 
-	time.sleep(5) #to show the results for 5 seconds before the program exits!
+	time.sleep(3) #to show the results for 3 seconds before the program exits!
