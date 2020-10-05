3d61512f068e2acbf573663daf49b8dda191bcd8
Anshul <72144591+Anshul-07@users.noreply.github.com>
2020-10-05 14:05:08 +0530

    This is simpler code then your code.
    Why this code is easier? #Read below
    1) In these code there is no requirement for any module.
    2) This code can be understand by a beginner.
    3) This code is smaller.

--- Main.py
+++ Main.py
@@ -1,53 +1,19 @@
-import time
+# Simple calculator in Python
+i=int(input("Enter 1st number : "))
+v=int(input("Enter 2nd number : "))
+o=input("Enter operator(+,-,*,/,**,%): ")
 
-#add two numbers 
-def add(num1, num2): 
-	return num1 + num2 
-
-#subtract two numbers 
-def subtract(num1, num2): 
-	return num1 - num2 
-
-#multiply two numbers 
-def multiply(num1, num2): 
-	return num1 * num2 
-
-#divide two numbers 
-def divide(num1, num2): 
-	return num1 / num2 
-
-print("Please select operation -\n"
-		"1. Add\n"
-		"2. Subtract\n" 
-		"3. Multiply\n"
-		"4. Divide\n") 
-
-
-# Taking input from the user 
-select = input("Select operations form 1, 2, 3, 4 :") 
-
-number_1 = int(input("Enter first number: ")) 
-number_2 = int(input("Enter second number: ")) 
-
-if select == '1': 
-	print(number_1, "+", number_2, "=", 
-					add(number_1, number_2))
-	time.sleep(5) #to show the results for 5 seconds before the program exits!
-
-elif select == '2': 
-	print(number_1, "-", number_2, "=", 
-					subtract(number_1, number_2)) 
-	time.sleep(5) #to show the results for 5 seconds before the program exits!
-
-elif select == '3': 
-	print(number_1, "*", number_2, "=", 
-					multiply(number_1, number_2)) 
-	time.sleep(5) #to show the results for 5 seconds before the program exits!
-
-elif select == '4': 
-	print(number_1, "/", number_2, "=", 
-					divide(number_1, number_2)) 
-	time.sleep(5) #to show the results for 5 seconds before the program exits!
-else: 
-	print("Syntax Error!") 
-	time.sleep(5) #to show the results for 5 seconds before the program exits!
+if o=="+":
+    print("Your aanswer is:", i+v)    
+elif o=="-":
+    print("Your answer is:", i-v)
+elif o=="/":
+     print("Your answer is:", i/v)
+elif o=="*":
+     print("Your answer is:", i*v)
+elif o=="**":
+     print("Your answer is:", i**v)
+elif o=="%":
+     print("Your answer is:", i%v)
+else :
+    print("Invalid operation")
